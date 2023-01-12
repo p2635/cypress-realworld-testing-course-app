fd9d8e210ce43f50bd97e4bd4dca2bfc4d0a95e9
PhilAtKC <philip.wong@kinandcarta.com>
2023-01-12 12:52:45 +0000

    update test with getByData

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,10 +4,10 @@ describe('home page', () => {
   })
 
   it('the h1 contains the a title', () => {
-   cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+   cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
   })
 
-  it.only("the features text on the homepage are correct", () => {
+  it("the features text on the homepage are correct", () => {
     cy.get("dt").eq(0).contains("4 Courses")
     cy.get("dt").eq(1).contains("25+ Lessons")
     cy.get("dt").eq(2).contains("Free and Open Source")
