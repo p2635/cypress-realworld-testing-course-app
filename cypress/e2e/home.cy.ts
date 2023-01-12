f8daddf55f3594b88a133fd828e4c8ef18acb9b8
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-12 15:15:20 +0000

    how-to-test-multiple-pages complete

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,13 +3,33 @@ describe('home page', () => {
     cy.visit("http://localhost:3000")
   })
 
-  it('the h1 contains the a title', () => {
-   cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+  context("Hero section", () => {
+    it('the h1 contains the a title', () => {
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+     })
+   
+    it("the features text on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features text on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context.only("Courses section", () => {
+    it("Accessing the Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
+
+    it("Accessing the Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-foundations")
+    })
+
+    it("Accessing the Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/cypress-fundamentals")
+    })
   })
+
 })
