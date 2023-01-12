9a1a189c8d3606f3aef0d2cd6630fd77e47a35f7
PhilAtKC <philip.wong@kinandcarta.com>
2023-01-12 12:50:09 +0000

    add custom cy command for data types

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,10 @@
 //     }
 //   }
 // }
+
+/// <reference types="cypress" />
+
+Cypress.Commands.add("getByData", (selector) => {
+    return cy.get(`[data-test=${selector}]`)
+  })
+  
---
+++ cypress/support/index.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+    interface Chainable {
+      getByData(dataTestAttribute: string): Chainable<JQuery<HTMLElement>>
+    }
+  }
+  
